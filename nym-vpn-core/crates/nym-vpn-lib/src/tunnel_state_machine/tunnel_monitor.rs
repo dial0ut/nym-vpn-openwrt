63f06613131f8470d0d16236dea0524eb1cc3d85
ZM <github.dandelion453@passinbox.com>
2026-09-22 15:20:19 -0500

    sync: pass monitor shutdown token to bandwidth controller
    nymtech/nym-vpn-client #5685

--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tunnel_monitor.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tunnel_monitor.rs
@@ -1089,7 +1089,7 @@ impl TunnelMonitor {
             entry_signal_rx,
             exit_signal_rx,
             gw_update_version,
-            self.shutdown_token.child_token(),
+            self.shutdown_token.clone(),
         );
 
         let authenticator_listener_handle = match authenticator_listener_handle {
